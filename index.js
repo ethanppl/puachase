6e441a925ffb5ec712b8b6ccf8bc0724d3824d94
Ethan <43922849+ethanppl@users.noreply.github.com>
2024-09-22 01:10:59 +0800

    Mobile drag and drop

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const getPua = ({puaX, puaY, puaFile}) => {
   pua.style.height = "40px";
   pua.style.position = "fixed";
 
-  pua.style.zIndex = "1";
+  pua.style.zIndex = "10";
   pua.style.backgroundImage = `url(${puaFile})`;
 
   return pua;
@@ -21,14 +21,14 @@ const getBall = ({ballX, ballY, ballFile}) => {
 
   ball.id = 'ball';
 
-  ball.style.left = `${ballX}px`;
-  ball.style.top = `${ballY}px`;
+  ball.style.left = `${ballX - 10}px`;
+  ball.style.top = `${ballY - 10}px`;
 
   ball.style.width = "20px";
   ball.style.height = "20px";
   ball.style.position = "fixed";
 
-  ball.style.zIndex = "1";
+  ball.style.zIndex = "20";
   ball.style.backgroundImage = `url(${ballFile})`;
 
   return ball;
@@ -38,6 +38,8 @@ const init = () => {
   // Global variables to keep track of
   let mouseX = window.innerWidth / 2;
   let mouseY = window.innerHeight / 2;
+  let ballX = mouseX;
+  let ballY = mouseY;
   let puaX = 20;
   let puaY = 20;
 
@@ -48,18 +50,18 @@ const init = () => {
 
   // Assets
   const pua = getPua({puaX, puaY, puaFile});
-  const ball = getBall({ballX: mouseX, ballY: mouseY, ballFile});
+  const ball = getBall({ballX, ballY, ballFile});
 
   const animate = () => {
-    // Y axis: feet stick to the mouse
-    const diffY = mouseY - puaY - 40;
-    const transformY = diffY * SPEED;
-    puaY += transformY;
-
-    // X axis: body sit next to the mouse
-    const diffX = mouseX > puaX ? mouseX - puaX - 40 : mouseX - puaX;
+    // X axis: body sit next to the ball
+    const diffX = ballX > puaX ? ballX - puaX - 40 : ballX - puaX;
     const transformX = diffX * SPEED;
     puaX += transformX;
+
+    // Y axis: feet stick to the mouse
+    const diffY = ballY - puaY - 40;
+    const transformY = diffY * SPEED;
+    puaY += transformY;
 
     pua.style.left = `${puaX}px`;
     pua.style.top = `${puaY}px`;
@@ -74,9 +76,25 @@ const init = () => {
     mouseX = event.clientX;
     mouseY = event.clientY; 
 
-    ball.style.left = `${event.clientX - 10}px`;
-    ball.style.top = `${event.clientY - 10}px`;
+    ballX = mouseX;
+    ballY = mouseY;
+    ball.style.left = `${ballX - 10}px`;
+    ball.style.top = `${ballY - 10}px`;
   });
+
+  // Prevent default behaviour in drag and drop
+  ball.ondragstart = () => false;
+
+  // Mobile: drag and drop
+  ball.ontouchmove = (event) => {
+    mouseX = event.touches[0].clientX;
+    mouseY = event.touches[0].clientY;
+
+    ballX = mouseX;
+    ballY = mouseY;
+    ball.style.left = `${ballX - 10}px`;
+    ball.style.top = `${ballY - 10}px`;
+  }
 
   window.requestAnimationFrame(animate);
 }
