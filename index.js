02400969cca96bcd413395d2f4804168ebc0cf8f
Ethan <43922849+ethanppl@users.noreply.github.com>
2024-09-22 00:36:40 +0800

    Add ball and blue background

--- index.js
+++ index.js
@@ -16,19 +16,39 @@ const getPua = ({puaX, puaY, puaFile}) => {
   return pua;
 }
 
+const getBall = ({ballX, ballY, ballFile}) => {
+  const ball = document.createElement('div');
+
+  ball.id = 'ball';
+
+  ball.style.left = `${ballX}px`;
+  ball.style.top = `${ballY}px`;
+
+  ball.style.width = "20px";
+  ball.style.height = "20px";
+  ball.style.position = "fixed";
+
+  ball.style.zIndex = "1";
+  ball.style.backgroundImage = `url(${ballFile})`;
+
+  return ball;
+}
+
 const init = () => {
   // Global variables to keep track of
-  let mouseX = 20;
-  let mouseY = 60;
+  let mouseX = window.innerWidth / 2;
+  let mouseY = window.innerHeight / 2;
   let puaX = 20;
   let puaY = 20;
 
   // Params
   const SPEED = 0.05;
   const puaFile = "./pua-sitting.png";
+  const ballFile = "./ball.png";
 
   // Assets
   const pua = getPua({puaX, puaY, puaFile});
+  const ball = getBall({ballX: mouseX, ballY: mouseY, ballFile});
 
   const animate = () => {
     // Y axis: feet stick to the mouse
@@ -48,10 +68,14 @@ const init = () => {
   }
 
   document.body.appendChild(pua);
+  document.body.appendChild(ball);
 
   document.addEventListener('mousemove', (event) => {
     mouseX = event.clientX;
     mouseY = event.clientY; 
+
+    ball.style.left = `${event.clientX - 10}px`;
+    ball.style.top = `${event.clientY - 10}px`;
   });
 
   window.requestAnimationFrame(animate);
