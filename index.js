dea814199bb077476ebdb74f8e06169b5fdd9996
Ethan <43922849+ethanppl@users.noreply.github.com>
2024-09-23 00:57:55 +0800

    Container around ball for easier dragging in mobile

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-const getPua = ({puaX, puaY, puaFile}) => {
-  const pua = document.createElement('div');
-
-  pua.id = 'pua';
-
-  pua.style.left = `${puaX}px`;
-  pua.style.top = `${puaY}px`;
-
-  pua.style.width = "40px";
-  pua.style.height = "40px";
-  pua.style.position = "fixed";
-
-  pua.style.zIndex = "10";
-  pua.style.backgroundImage = `url(${puaFile})`;
-
-  return pua;
-}
-
-const getBall = ({ballX, ballY, ballFile}) => {
-  const ball = document.createElement('div');
-
-  ball.id = 'ball';
-
-  ball.style.left = `${ballX - 10}px`;
-  ball.style.top = `${ballY - 10}px`;
-
-  ball.style.width = "20px";
-  ball.style.height = "20px";
-  ball.style.position = "fixed";
-
-  ball.style.zIndex = "20";
-  ball.style.backgroundImage = `url(${ballFile})`;
-
-  return ball;
-}
-
 const init = () => {
   // Global variables to keep track of
   let mouseX = window.innerWidth / 2;
@@ -45,8 +9,73 @@ const init = () => {
 
   // Params
   const SPEED = 0.05;
+
+  // Constants: easier to change the size of the elements if needed
+  const PUA_WIDTH = 40;
+  const PUA_HEIGHT = 40;
   const puaFile = "./pua-sitting.png";
+
+  const BALL_WIDTH = 100;
+  const BALL_HEIGHT = 100;
+  const BALL_IMAGE_WIDTH = 20;
+  const BALL_IMAGE_HEIGHT = 20;
   const ballFile = "./ball.png";
+
+  const getPua = ({puaX, puaY, puaFile}) => {
+    // Pua Image
+    const pua = document.createElement('div');
+    pua.id = 'pua';
+
+    // Fixed position in the page
+    pua.style.position = "fixed";
+    pua.style.left = `${puaX}px`;
+    pua.style.top = `${puaY}px`;
+    pua.style.width = `${PUA_WIDTH}px`;
+    pua.style.height = `${PUA_HEIGHT}px`;
+
+    // Higher z-index than the default
+    pua.style.zIndex = "10";
+    pua.style.backgroundImage = `url(${puaFile})`;
+
+    return pua;
+  }
+
+  const getBall = ({ballX, ballY, ballFile}) => {
+    // Ball Image
+    const ball = document.createElement('div');
+    ball.id = 'ball';
+
+    // Relative within the ball container
+    ball.style.position = "relative";
+    ball.style.width = `${BALL_IMAGE_WIDTH}px`;
+    ball.style.height = `${BALL_IMAGE_HEIGHT}px`;
+
+    // Higher z-index than pua
+    ball.style.zIndex = "20";
+    ball.style.backgroundImage = `url(${ballFile})`;
+
+    // Ball Container for better dragging
+    const ballContainer = document.createElement('div');
+    ballContainer.id = 'ball-container';
+
+    // Fixed position in the page
+    ballContainer.style.position = "fixed";
+    ballContainer.style.left = `${ballX - (BALL_WIDTH / 2)}px`;
+    ballContainer.style.top = `${ballY - (BALL_HEIGHT / 2)}px`;
+    ballContainer.style.width = `${BALL_WIDTH}px`;
+    ballContainer.style.height = `${BALL_HEIGHT}px`;
+
+    // Center the ball image within the container
+    ballContainer.style.display = "flex";
+    ballContainer.style.justifyContent = "center";
+    ballContainer.style.alignItems = "center";
+
+    // Higher z-index than pua and put the ball image inside
+    ballContainer.style.zIndex = "20";
+    ballContainer.appendChild(ball);
+
+    return ballContainer;
+  }
 
   // Assets
   const pua = getPua({puaX, puaY, puaFile});
@@ -54,12 +83,12 @@ const init = () => {
 
   const animate = () => {
     // X axis: body sit next to the ball
-    const diffX = ballX > puaX ? ballX - puaX - 40 : ballX - puaX;
+    const diffX = ballX > puaX ? ballX - puaX - PUA_WIDTH : ballX - puaX;
     const transformX = diffX * SPEED;
     puaX += transformX;
 
     // Y axis: feet stick to the mouse
-    const diffY = ballY - puaY - 40;
+    const diffY = ballY - puaY - PUA_HEIGHT;
     const transformY = diffY * SPEED;
     puaY += transformY;
 
@@ -78,8 +107,8 @@ const init = () => {
 
     ballX = mouseX;
     ballY = mouseY;
-    ball.style.left = `${ballX - 10}px`;
-    ball.style.top = `${ballY - 10}px`;
+    ball.style.left = `${ballX - (BALL_WIDTH / 2)}px`;
+    ball.style.top = `${ballY - (BALL_HEIGHT / 2)}px`;
   });
 
   // Prevent default behaviour in drag and drop
@@ -95,8 +124,8 @@ const init = () => {
 
     ballX = mouseX;
     ballY = mouseY;
-    ball.style.left = `${ballX - 10}px`;
-    ball.style.top = `${ballY - 10}px`;
+    ball.style.left = `${ballX - (BALL_WIDTH / 2)}px`;
+    ball.style.top = `${ballY - (BALL_HEIGHT / 2)}px`;
   }
 
   window.requestAnimationFrame(animate);
